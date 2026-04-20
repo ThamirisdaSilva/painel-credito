d7803e749a70bbfa30ddfe50fae62b7dac1c3dd4
ThamirisAdriano <adrianosthamiris@gmail.com>
2026-04-20 15:59:45 -0300

    aula-6-parte2

---
+++ src/app/components/solicitacoes/filtro-solicitacoes/filtro-solicitacoes.ts
@@ -0,0 +1,16 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-filtro-solicitacoes',
+  standalone: true,
+  templateUrl: './filtro-solicitacoes.html',
+  styleUrl: './filtro-solicitacoes.scss'
+})
+export class FiltroSolicitacoesComponent {
+
+  @Output() filtroChange = new EventEmitter<string>();
+
+  selecionar(status: string) {
+    this.filtroChange.emit(status);
+  }
+}
