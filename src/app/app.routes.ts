308d5af0ce70421d933ce18bd3f7236884a63e9a
ThamirisAdriano <adrianosthamiris@gmail.com>
2026-04-24 10:49:31 -0300

    aula9-a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,25 +5,51 @@ export const routes: Routes = [
   {
     path: '',
     redirectTo: 'login',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
+
   {
     path: 'login',
-    loadComponent: () =>
-      import('./components/login/login/login')
-        .then(m => m.LoginComponent)
+    loadComponent: () => import('./components/login/login/login').then((m) => m.LoginComponent),
   },
+
   {
     path: 'solicitacoes',
     loadComponent: () =>
-      import('./components/solicitacoes/solicitacao-lista/solicitacao-lista')
-        .then(m => m.SolicitacaoListaComponent),
-    canActivate: [authGuard]
+      import('./components/solicitacoes/solicitacao-lista/solicitacao-lista').then(
+        (m) => m.SolicitacaoListaComponent,
+      ),
+    canActivate: [authGuard],
   },
+
+  // ROTAS ESPECÍFICAS PRIMEIRO
+
   {
-  path: 'solicitacoes/:id',
-  loadComponent: () =>
-    import('./components/solicitacoes/solicitacao-detalhe/solicitacao-detalhe')
-      .then(m => m.SolicitacaoDetalheComponent)
-},
+    path: 'solicitacoes/nova',
+    loadComponent: () =>
+      import('./components/solicitacoes/solicitacao-form/solicitacao-form').then(
+        (m) => m.SolicitacaoFormComponent,
+      ),
+    canActivate: [authGuard],
+  },
+
+  {
+    path: 'solicitacoes/editar/:id',
+    loadComponent: () =>
+      import('./components/solicitacoes/solicitacao-form/solicitacao-form').then(
+        (m) => m.SolicitacaoFormComponent,
+      ),
+    canActivate: [authGuard],
+  },
+
+  // ROTA GENÉRICA SEMPRE POR ÚLTIMO
+
+  {
+    path: 'solicitacoes/:id',
+    loadComponent: () =>
+      import('./components/solicitacoes/solicitacao-detalhe/solicitacao-detalhe').then(
+        (m) => m.SolicitacaoDetalheComponent,
+      ),
+    canActivate: [authGuard],
+  },
 ];
--- src/app/components/solicitacoes/solicitacao-item/solicitacao-item.ts
+++ src/app/components/solicitacoes/solicitacao-item/solicitacao-item.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SolicitacaoViewModel } from '../../../services/graphql.service';
 import { Router } from '@angular/router';
+
+import { SolicitacaoViewModel } from '../../../services/graphql.service';
+import { SolicitacoesFacade } from '../../../services/solicitacoes.facade';
 
 @Component({
   selector: 'app-solicitacao-item',
@@ -9,14 +11,25 @@ import { Router } from '@angular/router';
   imports: [CommonModule],
   templateUrl: './solicitacao-item.html',
   styleUrls: ['./solicitacao-item.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SolicitacaoItemComponent {
   @Input({ required: true }) solicitacao!: SolicitacaoViewModel;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private facade: SolicitacoesFacade,
+  ) {}
 
-irParaDetalhe() {
-  this.router.navigate(['/solicitacoes', this.solicitacao.id]);
-}
+  irParaDetalhe() {
+    this.router.navigate(['/solicitacoes', this.solicitacao.id]);
+  }
+
+  editar() {
+    this.router.navigate(['/solicitacoes/editar', this.solicitacao.id]);
+  }
+
+  deletar() {
+    this.facade.deletar(this.solicitacao.id);
+  }
 }
