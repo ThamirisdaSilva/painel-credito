a90e55af2316088ccea37b4b9ed0c5e202590a5f
ThamirisAdriano <adrianosthamiris@gmail.com>
2026-04-29 17:14:18 -0300

    aula12-final

--- src/app/components/solicitacoes/resumo-solicitacoes/resumo-solicitacoes.ts
+++ src/app/components/solicitacoes/resumo-solicitacoes/resumo-solicitacoes.ts
@@ -1,23 +1,25 @@
 import { Component, computed, input } from '@angular/core'; // Importe o 'input'
 import { SolicitacaoViewModel } from '../../../services/graphql.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-resumo-solicitacoes',
+  imports: [TranslatePipe],
   standalone: true,
-  templateUrl: './resumo-solicitacoes.html'
+  templateUrl: './resumo-solicitacoes.html',
 })
 export class ResumoSolicitacoesComponent {
-  // Transforma em Signal Input
-  lista = input<SolicitacaoViewModel[]>([]); 
+  lista = input<SolicitacaoViewModel[]>([]);
 
   resumo = computed(() => {
-    const dados = this.lista(); 
+    const dados = this.lista();
 
     return {
       total: dados.length,
-      aprovadas: dados.filter(i => i.status === 'aprovado').length,
-      pendentes: dados.filter(i => i.status === 'pendente').length,
-      recusadas: dados.filter(i => i.status === 'recusado').length
+      aprovadas: dados.filter((i) => i.status === 'aprovado').length,
+      pendentes: dados.filter((i) => i.status === 'pendente').length,
+      recusadas: dados.filter((i) => i.status === 'recusado').length,
+      em_analise: dados.filter((i) => i.status === 'em_analise').length,
     };
   });
 }
